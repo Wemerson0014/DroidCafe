a9e34dec27e9dd99fbe5c9fb1c6e43b73f2edcaf
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2021-03-19 20:06:20 -0300

    Cria recursos de strings, adiciona método para exibição de um toast para cada item do radio button clicado, altera tipos de entradas e correções no layout activity_order

--- app/src/main/java/com/estudo/droidcafe/OrderActivity.java
+++ app/src/main/java/com/estudo/droidcafe/OrderActivity.java
@@ -1,10 +1,12 @@
 package com.estudo.droidcafe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -14,6 +16,7 @@ public class OrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order);
 
         showDessertOrder();
+        onRadioButtonClicked();
     }
 
     private void showDessertOrder() {
@@ -21,5 +24,25 @@ public class OrderActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String message = "Order: " + intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         text_order.setText(message);
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+    }
+
+    private void onRadioButtonClicked() {
+        RadioGroup radioGroup = findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+
+            if (checkedId == R.id.radio_same_day) {
+                showToast(getString(R.string.radio_same_day));
+            }
+            if (checkedId == R.id.radio_next_day) {
+                showToast(getString(R.string.radio_next_day));
+            }
+            if (checkedId == R.id.radio_pick_up) {
+                showToast(getString(R.string.radio_pick_up));
+            }
+        });
     }
 }
